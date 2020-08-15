0c48e2506fcc2fa1313af6a26646679e17f013c7
Chamon562 <Chamon562@gmail.com>
2020-08-14 23:31:45 -0700

    [weapons] Weapons.ejs is connected

--- routes/weapon.js
+++ routes/weapon.js
@@ -7,12 +7,28 @@ const axios = require('axios')
 router.get('/', (req, res)=>{
     db.weapon.findAll()
     .then(findWeapon =>{
-        res.render(findWeapon)
+        res.render('weapons', {findWeapon})
     })
     .catch(error =>{
         console.log('ERROR weapon.js', error)
     })
 })
+
+// router.get('/:type', (req, res)=>{
+//     let type = req.params.type
+//     let weaponUrl = `https://mhw-db.com/weapons/${type}`
+//     axios.get(weaponUrl)
+//     .then(function(apiResponse){
+        
+//         let weapon = apiResponse.data
+//         console.log(weapon)
+//         res.render('weapons', {weapon})
+//     })
+//     .catch(error =>{
+//         console.log('error', error)
+//         res.render('error')
+//     })
+// })
 
 
 
